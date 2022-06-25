5f693e227e01824a1db35705f53641ca482f7e3d
DenisMuzyka <demuz.prog@gmail.com>
2022-06-26 00:33:23 +0300

    task PP_2_2_1 is done

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -16,21 +16,25 @@ public class MainApp {
 
       UserService userService = context.getBean(UserService.class);
 
+
       userService.add(new User("User1", "Lastname1", "user1@mail.ru"), new Car("audi", 3));
       userService.add(new User("User2", "Lastname2", "user2@mail.ru"), new Car("bmw", 5));
-//      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-//      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
 
+
+      // пользователи с авто
       List<User> users = userService.listUsers();
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
-         System.out.println("Car = "+user.getCar());
-         System.out.println();
+         if (user.getCar() != null) {
+            System.out.println("Id = "+user.getId());
+            System.out.println("First Name = "+user.getFirstName());
+            System.out.println("Last Name = "+user.getLastName());
+            System.out.println("Email = "+user.getEmail());
+            System.out.println("Car = "+user.getCar());
+            System.out.println();
+         }
       }
 
+      // пользователь, выбранный по модели и серии машины
       userService.getUserFromParam("audi", 3);
       context.close();
    }
